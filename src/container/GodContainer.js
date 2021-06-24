c73781765a8ce46f08fd777a510e13baa2df0ab4
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-23 23:26:30 -0400

    Updating the container file

--- src/container/GodContainer.js
+++ src/container/GodContainer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import GodCollection from '../components/GodCollection';
+import { Container } from 'semantic-ui-react';
 
 class GodContainer extends React.Component {
 
     state = {
-        gods: [],
-        
+        gods: [],  
     }
 
     componentDidMount(){
         fetch('http://localhost:5000/gods')
         .then(response => response.json())
-        .then(data => this.setState({
-            gods: data
-        }))
+        .then(parsed => this.setState({gods: parsed}))
     }
+
+  
 
    /* godsToRender = () => {
         return this.state.gods.map((god) => {
@@ -30,8 +30,11 @@ render(){
         <>
             <h1>Welcome to the Gods!</h1>
             <h2><a href="http://localhost:3000/form">Click here to add a God!</a></h2>
-            <GodCollection />
-            {/* {this.godsToRender()} */}
+            <Container>
+                <GodCollection gods={this.state.gods} />
+                {/* {this.godsToRender()} */}
+            </Container>
+            
         </>
     )
 
