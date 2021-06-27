f2c297ba2629b655710fac4a9afc4ee682c55361
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-27 01:03:37 -0400

    added new route paths to app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GodContainer from './container/GodContainer'
 import GodForm from './components/GodComponents/GodForm'
+import HeroForm from './components/HeroComponents/HeroForm'
 import HeroContainer from './container/HeroContainer';
+import HomeContainer from './container/HomeContainer'
 
 function App() {
   return (
     <div className="App">
      <Router>
        <Switch>
+        <Route path="/" exact component={HomeContainer} />  
          <Route path="/gods" exact component={GodContainer} />
-         <Route path="/form"  exact component={GodForm} />
+         <Route path="/godform"  exact component={GodForm} />
+         <Route path="/heroform"  exact component={HeroForm} />
          <Route path="/heroes" exact component={HeroContainer} />
        </Switch>
      </Router>
