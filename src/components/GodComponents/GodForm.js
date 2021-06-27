1bf6a9aaf0f28b1b853460a5a57f45232c5c6544
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-26 23:38:54 -0400

    Updating the contents of the GodForm

--- src/components/GodComponents/GodForm.js
+++ src/components/GodComponents/GodForm.js
@@ -12,6 +12,7 @@ class GodForm extends React.Component {
         father: '',
         mother: '',
         power: '',
+        description: ''
     }
 
     handleFormSubmit = (godCollection) => {
@@ -67,6 +68,9 @@ class GodForm extends React.Component {
     handleURLChange = (event) => {
         this.setState({url: event.target.value})
     }
+    handleDescriptionChange = (event) => {
+        this.setState({description: event.target.value})
+    }
 
     handleFormSubmit = (event) => {
         event.preventDefault();
@@ -79,15 +83,17 @@ render() {
             <h3>Add A God!</h3>
             <Form onSubmit={this.handleFormSubmit}>
                 <Form.Group widths="equal">
+                    <Form.Input fluid label="Image URL" placeholder="url" name="url" value={this.state.url} onChange={this.handleURLChange} />
                     <Form.Input fluid label="Name" placeholder="Name" name="name"  value={this.state.name} onChange={this.handleNameChange} />
                     <Form.Input fluid label="Roman Name" placeholder="Roman Name" name="romanname" value={this.state.romanname} onChange={this.handleRomanNameChange} />
                     <Form.Input fluid label="Symbol" placeholder="Symbol" name="symbol" value={this.state.symbol} onChange={this.handleSymbolChange} />
                     <Form.Input fluid label="Father" placeholder="Father" name="father" value={this.state.father} onChange={this.handleFatherChange} />
                     <Form.Input fluid label="Mother" placeholder="Mother" name="mother" value={this.state.mother} onChange={this.handleMotherChange} />
                     <Form.Input fluid label="Power" placeholder="Power" name="power" value={this.state.power} onChange={this.handlePowerChange} />
-                    <Form.Input fluid label="Image URL" placeholder="url" name="url" value={this.state.url} onChange={this.handleURLChange} />
+                    <Form.Input fluid label="Description" placeholder="Description" name="description" value={this.state.description} onChange={this.handleDescriptionChange} />   
                 </Form.Group>
                 <Form.Button>Submit</Form.Button>
+                <Form.Button><a href="http://localhost:3000/gods">Back</a></Form.Button>
                 <Form.Button><a href="http://localhost:3000/">Home</a></Form.Button>
             </Form>
         </div>
