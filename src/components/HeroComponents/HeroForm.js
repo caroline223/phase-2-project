916f10d3cb0d89878f1c49acf96bec485b9b5fe2
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-06-29 01:47:24 -0400

    abstracted the handleOnChange functionality for the HeroForm.js file

--- src/components/HeroComponents/HeroForm.js
+++ src/components/HeroComponents/HeroForm.js
@@ -41,36 +41,13 @@ class HeroForm extends React.Component {
         }
     }
 
-
-    handleNameChange = (event) => {
-        this.setState({name : event.target.value})
+    handleOnChange = (event) => {
+        console.log(this.state)
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
-    handleHeroTypeChange = (event) => {
-        this.setState({romanname : event.target.value})
-    }
-
-    handleHomeChange = (event) => {
-        this.setState({symbol : event.target.value})
-    }
-
-    handleFatherChange = (event) => {
-        this.setState({father : event.target.value})
-    }
-
-    handleMotherChange = (event) => {
-        this.setState({mother : event.target.value})
-    }
-
-    handlePowerChange = (event) => {
-        this.setState({power : event.target.value})
-    }
-    handleURLChange = (event) => {
-        this.setState({url: event.target.value})
-    }
-    handleDescriptionChange = (event) => {
-        this.setState({description: event.target.value})
-    }
 
     handleFormSubmit = (event) => {
         event.preventDefault();
@@ -83,14 +60,14 @@ render() {
             <h3>Build A Hero</h3>
             <Form onSubmit={this.handleFormSubmit}>
                 <Form.Group widths="equal">
-                    <Form.Input fluid label="Image URL" placeholder="url" name="url" value={this.state.url} onChange={this.handleURLChange} />
-                    <Form.Input fluid label="Name" placeholder="Name" name="name"  value={this.state.name} onChange={this.handleNameChange} />
-                    <Form.Input fluid label="Hero Type" placeholder="Hero Type" name="herotype" value={this.state.herotype} onChange={this.handleHeroTypeChange} />
-                    <Form.Input fluid label="Home" placeholder="Home" name="home" value={this.state.home} onChange={this.handleHomeChange} />
-                    <Form.Input fluid label="Father" placeholder="Father" name="father" value={this.state.father} onChange={this.handleFatherChange} />
-                    <Form.Input fluid label="Mother" placeholder="Mother" name="mother" value={this.state.mother} onChange={this.handleMotherChange} />
-                    <Form.Input fluid label="Power" placeholder="Power" name="power" value={this.state.power} onChange={this.handlePowerChange} />
-                    <Form.Input fluid label="Description" placeholder="Description" name="description" value={this.state.description} onChange={this.handleDescriptionChange} />
+                    <Form.Input fluid label="Image URL" placeholder="url" name="url" value={this.state.url} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Name" placeholder="Name" name="name"  value={this.state.name} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Hero Type" placeholder="Hero Type" name="herotype" value={this.state.herotype} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Home" placeholder="Home" name="home" value={this.state.home} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Father" placeholder="Father" name="father" value={this.state.father} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Mother" placeholder="Mother" name="mother" value={this.state.mother} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Power" placeholder="Power" name="power" value={this.state.power} onChange={this.handleOnChange} />
+                    <Form.Input fluid label="Description" placeholder="Description" name="description" value={this.state.description} onChange={this.handleOnChange} />
                 </Form.Group>
                 <Form.Button>Submit</Form.Button>
                 <Form.Button><a href="http://localhost:3000/heroes">Back</a></Form.Button>
